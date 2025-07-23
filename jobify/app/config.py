5add0e992e90b760a569d3ea587ae09bda90c976
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-07-23 13:05:25 +0000

    build a config file for loadin .env

---
+++ jobify/app/config.py
@@ -0,0 +1,14 @@
+# app/config.py
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # load variables from .env
+
+class Settings:
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
+    ALGORITHM: str = os.getenv("ALGORITHM")
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
+
+settings = Settings()
