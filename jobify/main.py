ad0e7f8b3104a85ef61f853f0b06065d7a3c834c
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-07-23 13:14:50 +0000

    built pasword hashing utility

--- jobify/main.py
+++ jobify/main.py
@@ -2,6 +2,11 @@
 
 from fastapi import FastAPI
 from app.routes import user
+from app.database import Base, engine
+from app.models import user as user_model  # This ensures the model is registered
+
+# Create tables if they don't exist
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Jobify Backend")
 
@@ -11,4 +16,5 @@ app.include_router(user.router, prefix="/api/users", tags=["Users"])
 @app.get("/")
 def home():
     return {"message": "Welcome to Jobify!"}
+
 
